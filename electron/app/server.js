4fc72440c8f4f7cabac13730ae5adf0fa6a7bbbe
Alex <10469240+afmhenry@users.noreply.github.com>
2022-01-09 18:48:21 +0100

    started building electron interface, with express backend.

---
+++ electron/app/server.js
@@ -0,0 +1,46 @@
+const express     = require('express');
+const path     = require('path');
+const ffmpegPath  = require('ffmpeg-static');
+Stream            = require('node-rtsp-stream');
+const {spawn} = require('child_process');
+
+
+let app = express();
+
+app.listen(3000, () => {
+  console.log(`Example app listening`)
+})
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
+app.get('/test', function(req, res){
+    res.send({"Server":"Running"});
+});
+
+app.get('/query', function(req, res){
+
+  var script_response = ""
+  const script_process = spawn('python', ['test.py']);
+  script_process.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    script_response = data.toString();
+  });
+    res.send({"Server":script_response});
+});
+
+
+app.get('/query2', function(req, res){
+
+  var script_response = "";
+  const script_process = spawn('bean-query',["beans/alex.beancount", "select sum(cost(position)) as total, month, year where account ~ \"Expenses:Consumption.*\" and not account ~\".*Tax.*\"  and year=2021 group by year, month order by year, month DESC"]);
+  script_process.stdout.on("data", data => {
+      res.send({"Server":data.toString()});
+  });
+  
+});
+
+// Handle stop signals
+const exitfn = function () {
+    process.exit(0);
+};
+process.on("SIGINT", exitfn);
+process.on("SIGTERM", exitfn);

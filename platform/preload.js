d96fdcc8acdae5046f814eb563b9e643fd5cf3cb
Alex <10469240+afmhenry@users.noreply.github.com>
2022-01-11 02:00:28 +0100

    react running now

---
+++ platform/preload.js
@@ -0,0 +1,16 @@
+const { exec } = require("child_process");
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+
+  //exec(`npm run-script dev`);
+  //exec(`npm run-script client`);
+
+})
